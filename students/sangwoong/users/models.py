219bba4d0a39f870a474faf1c40b33dbe3bf0ccf
sangwoong03 <sangwoong03@DESKTOP-19GVLRN.localdomain>
2022-06-11 12:47:10 +0900

    Add: completed modelling of users

---
+++ students/sangwoong/users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model) :
+  last_name     = models.CharField(max_length=16) 
+  first_name    = models.CharField(max_length=45)
+  email         = models.EmailField(max_length=254)
+  account       = models.CharField(max_length=45)
+  password      = models.CharField(max_length=128)
+  phone_number  = models.CharField(max_length=32)
+  date_of_birth = models.DateField()
+  
+  class Meta :
+    db_table = "users"
