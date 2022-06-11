d7d224b8d5cd41c96f543863855ba3f5f5289dce
kjy0627 <jkim8@stevens.edu>
2022-06-11 17:15:23 +0900

    Add:users

---
+++ students/jiyeongkim/westagram/users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    name            = models.TextField(max_length=45)
+    email           = models.CharField(max_length=45, unique=True)
+    Password        = models.CharField(max_length=45,null=True,default='')
+    line            = models.IntegerField(unique=True)
+
+    class Meta:
+        db_table = 'users'
+
+
