950f8266bf1e851ceb9f4fda20e85f0b9ccc4161
kjy0627 <jkim8@stevens.edu>
2022-06-13 14:05:09 +0900

    Fix:models_

--- students/jiyeongkim/westagram/users/models.py
+++ students/jiyeongkim/westagram/users/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class User(models.Model):
-    name            = models.TextField(max_length=45)
+    first_name      = models.TextField(max_length=45)
+    last_name       = models.TextField(max_length=45)
     email           = models.CharField(max_length=45, unique=True)
     Password        = models.CharField(max_length=45, null=False)
     line            = models.IntegerField(unique=True)
 
     class Meta:
         db_table = 'user'
-
-
