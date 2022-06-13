8dbd552d81379d3243677ecb23115b6f4711c224
dev-taekwonlee <dev.taekwonlee@gmail.com>
2022-06-13 22:05:53 +0900

    Add: '/signup' in urlpatterns (users/urls.py) Update: 'class SignUpView' (users/views.py) Update: 'westagram' in urlpatterns (urls.py)

--- students/taekwon/users/models.py
+++ students/taekwon/users/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-  first_name    = models.CharField(max_length=45)
-  last_name     = models.CharField(max_length=45)
-  user_name     = models.CharField(max_length=50, unique=True)
-  email         = models.EmailField(max_length=100, unique=True)
-  password      = models.CharField(max_length=30)
-  phone_number  = models.CharField(max_length=20, unique=True)
-  date_of_birth = models.DateField()
+    first_name    = models.CharField(max_length=45)
+    last_name     = models.CharField(max_length=45)
+    user_name     = models.CharField(max_length=50, unique=True)
+    email         = models.EmailField(max_length=100, unique=True)
+    password      = models.CharField(max_length=30)
+    phone_number  = models.CharField(max_length=20, unique=True)
+    date_of_birth = models.DateField()
 
-  class Meta:
-    db_table = 'users'
+    class Meta:
+        db_table = 'users'
--- students/taekwon/users/views.py
+++ students/taekwon/users/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from dataclasses import dataclass
 
-# Create your views here.
+import json
+import re
+
+from django.http import JsonResponse
+from django.views import View
+
+from users.models import User
+
+class SignUpView(View):
+    def post(self, request):
+        try:
+            data        = json.loads(request.body)
+            first_name      = data['first_name']
+            last_name       = data['last_name']
+            user_name       = data['user_name']
+            email           = data['email']
+            password        = data['password']
+            phone_number    = data['phone_number']
+            date_of_birth   = data['date_of_birth']
+
+            EMAIL_VALIDATION    = "^[a-zA-z0-9+_.]+@[a-zA-z0-9-.]+\.[a-zA-z0-9-.]+$"
+            PASSWORD_VALIDATION = "^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[a-zA-Z\d@$!%*#?&]{8,}$"
+
+            if not re.match(EMAIL_VALIDATION, email):
+                return JsonResponse({'message': "INVALID_EMAIL"}, status=400)
+
+            if not re.match(PASSWORD_VALIDATION, password):
+                return JsonResponse({'message': "INVALID_PASSWORD"}, status=400)
+
+            if User.objects.filter(user_name = user_name).exists():
+                return JsonResponse({'message': "USER_NAME_ALREADY_IN_USE"}, status=400)
+
+            if User.objects.filter(email = email).exists():
+                return JsonResponse({'message': "EMAIL_ALREADY_IN_USE"}, status=400)
+
+            if User.objects.filter(phone_number = phone_number).exists():
+                return JsonResponse({'message': "PHONE_NUMBER_ALREADY_IN_USE"}, status=400)
+
+            User.objects.create(
+                first_name    = first_name,
+                last_name     = last_name,
+                user_name     = user_name,
+                email         = email,
+                password      = password,
+                phone_number  = phone_number,
+                date_of_birth = date_of_birth
+            )
+            return JsonResponse({'message': 'SUCCESS'}, status=201)
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
