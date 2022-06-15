747651643c06231a111f95c779e116614ef6e468
sangwoong03 <sangwoong03@DESKTOP-19GVLRN.localdomain>
2022-06-15 10:22:53 +0900

    UPDATE: moved bycrypt codes below the validation condition

--- students/sangwoong/users/views.py
+++ students/sangwoong/users/views.py
@@ -18,10 +18,6 @@ class SignUpView(View):
             password      = data["password"]
             phone_number  = data["phone_number"]
             date_of_birth = data["date_of_birth"]
-
-            encoded_password = password.encode("utf-8")
-            secret_password  = bcrypt.hashpw(encoded_password, bcrypt.gensalt())
-            decoded_password = secret_password.decode("utf-8")
             
             EMAIL_CHECK   = "^[a-zA-z0-9+_.]+@[a-zA-z0-9-.]+\.[a-zA-z0-9-.]+$"
             PW_CHECK      = "^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[a-zA-Z\d@$!%*#?&]{8,}$"
@@ -34,6 +30,10 @@ class SignUpView(View):
                 
             if User.objects.filter(email = email).exists() :
                 return JsonResponse({"Message": "EMAIL_ALREADY_EXIST"}, status=400)
+            
+            encoded_password = password.encode("utf-8")
+            secret_password  = bcrypt.hashpw(encoded_password, bcrypt.gensalt())
+            decoded_password = secret_password.decode("utf-8")
 
             User.objects.create(
                 last_name     = last_name,
