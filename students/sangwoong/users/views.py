b0e3abbd099b5b4a36bc7a9d80e260e729910c39
sangwoong03 <sangwoong03@DESKTOP-19GVLRN.localdomain>
2022-06-16 14:46:33 +0900

    UPDATE: modified LoginView based on reviews

--- students/sangwoong/users/views.py
+++ students/sangwoong/users/views.py
@@ -25,13 +25,13 @@ class SignUpView(View):
             PW_CHECK      = "^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[a-zA-Z\d@$!%*#?&]{8,}$"
             
             if not re.match(EMAIL_CHECK, email) :
-                return JsonResponse({"Message": "INVALID_EMAIL"}, status=400)
+                return JsonResponse({"message": "INVALID_EMAIL"}, status=400)
             
             if not re.match(PW_CHECK, password) :
-                return JsonResponse({"Message": "INVALID_PASSWORD"}, status=400)
+                return JsonResponse({"message": "INVALID_PASSWORD"}, status=400)
                 
             if User.objects.filter(email = email).exists() :
-                return JsonResponse({"Message": "EMAIL_ALREADY_EXIST"}, status=400)
+                return JsonResponse({"message": "EMAIL_ALREADY_EXIST"}, status=400)
             
             encoded_password = password.encode("utf-8")
             secret_password  = bcrypt.hashpw(encoded_password, bcrypt.gensalt())
@@ -58,20 +58,19 @@ class LoginView(View):
             data          = json.loads(request.body)
             user_account  = data["account"]
             user_password = data["password"]
-            user_id       = User.objects.get(account = user_account).id
-
-            if not User.objects.filter(account = user_account).exists() :
-                return JsonResponse({"Message": "INVALIDE_USER"}, status=401)
-
-            hashed_password = user_password.encode("utf-8")
-            saved_password  = User.objects.get(account = user_account).password
             
-            if not bcrypt.checkpw(hashed_password, saved_password.encode("utf-8")) :
-                return JsonResponse({"Message" : "INVALID_USER"}, status=401)
+            user           = User.objects.get(account = user_account)
+            input_password = user_password.encode("utf-8")
+            saved_password = user.password.encode("utf-8")
+            
+            if not bcrypt.checkpw(input_password, saved_password) :
+                return JsonResponse({"message" : "INVALID_USER"}, status=401)
 
-            access_token = jwt.encode({"user_id" : user_id}, SECRET_KEY, ALGORITHM)
+            access_token = jwt.encode({"user_id" : user.id}, SECRET_KEY, ALGORITHM)
 
-            return JsonResponse({"ACCESS_TOKEN": access_token, "Message": "SUCCESS"}, status=200)
-
+            return JsonResponse({"access_token": access_token, "message": "SUCCESS"}, status=200)
+        
         except KeyError:
-            return JsonResponse({"Message": "KEY_ERROR"}, status=400)
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+        except User.DoesNotExist:
+            return JsonResponse({"message": "INVALID_USER"}, status=401)
